a11680cb215033985aed95d5fe077fd4b9443059
matt <matgonzalez736@gmail.com>
2020-09-08 15:23:09 +0200

    rewrite entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ sls plugin install -n serverless-python-requirements
 sls deploy > sls.txt
 cd $GITHUB_WORKSPACE
 python3 app/endpoint.py /app/backend/api/sls.txt
-endpoint=$(cat app/backend/api/sls.txt)
+endpoint=$(cat /app/backend/api/sls.txt)
 echo "::set-output name=endpoint::$endpoint"
